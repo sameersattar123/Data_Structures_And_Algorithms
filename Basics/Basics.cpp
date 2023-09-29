196ceaa67590d5a42a40f12552179ee5e9049e01
Sameer <sameersattar1111@gmail.com>
2023-09-30 04:11:42 +0500

    second commit

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -3,4 +3,15 @@ using namespace std;
 
 int main(){
     cout << "sameer sattar shaikh" << endl;
+    int num = 33344;
+    cout << num << endl;
+    char character = 'A';
+    cout << character << endl;
+    float f = 33344.22;
+    cout << f << endl;
+    double d = 33344.22222;
+    cout << d << endl;
+    int size = sizeof(character);
+    cout << size << endl;
+
 }
