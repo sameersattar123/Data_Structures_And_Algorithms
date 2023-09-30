abdd7cd72839453c1303178c740f54469df9d521
Sameer <sameersattar1111@gmail.com>
2023-10-01 01:41:00 +0500

    basics  commit

--- Basics/Loops/WhileLoop.cpp
+++ Basics/Loops/WhileLoop.cpp
@@ -2,5 +2,35 @@
 using namespace std;
 
 int main(){
-    
+    // cout<< "Enter the value of Num"<<endl;
+    // int num;
+    // cin>> num;
+    // int initial = 1;
+    // while (initial <= num){
+    //     cout<<initial;
+    //     initial = initial + 1;
+    // }
+
+    // cout<< "Enter the value of Num"<<endl;
+    // int num;
+    // cin>> num;
+    // int sum = 0;
+    // int number = 1;
+    // while (number <= num){
+    //     sum = sum + number;
+    //     number = number + 1;
+    // }
+    // cout<<sum<<endl;
+
+    cout<< "Enter the value of Num"<<endl;
+    int num;
+    cin>> num;
+    int sum = 0;
+    int number = 2;
+    while (number <= num){
+        sum = sum + number;
+        number = number + 2;
+    }
+    cout<<sum<<endl;
+        
 }
