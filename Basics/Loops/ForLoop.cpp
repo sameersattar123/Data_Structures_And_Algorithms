510bf64e1c8c6f0c7d8bcef6c9a9fa93eb456b06
Sameer <sameersattar1111@gmail.com>
2023-10-01 04:40:47 +0500

    basic  commit

--- Basics/Loops/ForLoop.cpp
+++ Basics/Loops/ForLoop.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"Enter the Value of N"<<endl;
+int main()
+{
+    // cout<<"Enter the Value of N"<<endl;
+    // int n;
+    // cin>> n;
+    // for (int i = 1; i<=n; i++){
+    //     cout<<i<<endl;
+    // }
+
+    // sum of all number
+    // cout<<"Enter the Value of N"<<endl;
+    // int n;
+    // cin>> n;
+    // int sum = 0;
+    // for (int i = 1; i<=n; i++){
+    //     sum += i;
+    // }
+    // cout<<sum<<endl;
+
+    // fabonacci series
+    //  cout<<"Enter the Value of N"<<endl;
+    //  int n;
+    //  cin>> n;
+    //  int a = 0;
+    //  int b = 1;
+    //  cout<< a << " " << b << " ";
+    //  for (int i = 1; i <= n; i++){
+    //      int nextNum = a + b;
+    //      cout<<nextNum<< " ";
+    //      a = b;
+    //      b = nextNum;
+    //  }
+
+    // prime number
+    cout << "Enter the Value of N" << endl;
     int n;
-    cin>> n;
-    for (int i = 1; i<=n; i++){
-        cout<<i<<endl;
+    cin >> n;
+    bool isPrime = 1;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            isPrime = 0;
+            break;
+        }
     }
-    
+
+    if (isPrime == 0)
+    {
+        cout << "Not a Prime Number" << endl;
+    }
+    else
+    {
+        cout << "Prime Number" << endl;
+    }
 }
---
+++ Basics/Loops/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+ << "Not a Prime Number" << endl;
