66f5f13ab8b47c38045b2114148132a773c20edb
Sameer <sameersattar1111@gmail.com>
2023-10-04 05:15:24 +0500

    selection sort  algo commit

--- Algorithms/SelectionSort/SelectionSort.cpp
+++ Algorithms/SelectionSort/SelectionSort.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int selectionSort(int arr[] , int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i+1; j < n ; j++){
+               if(arr[j] < arr[minIndex]){
+                minIndex = j;
+               } 
+        }
+
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+    
+}
+
+void printArray(int arr[] , int size){
+    for (int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
 int main(){
-    cout<<"sameer"<<endl;
+    int arr[5] = {411,33,22,331,52};
+    selectionSort(arr , 5);
+    printArray(arr , 5);
 }
