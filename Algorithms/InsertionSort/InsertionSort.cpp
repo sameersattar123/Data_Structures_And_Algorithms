facb13c5ebf437d11b938de11fb271175be72933
Sameer <sameersattar1111@gmail.com>
2023-10-07 16:28:56 +0500

    insertion sort  commit

--- Algorithms/InsertionSort/InsertionSort.cpp
+++ Algorithms/InsertionSort/InsertionSort.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void insetionSort(int arr[] , int n){
+    for (int i = 1; i < n; i++){
+        int temp = arr[i];
+        int j = i -1;
+        for (; j >= 0; j--){
+            if (arr[j] > temp){
+                arr[j+1] = arr[j];
+            }else{
+                break;
+            }
+            
+        }
+        arr[j+1] = temp;
+        
+    }
+    
+}
+
+void printArray(int arr[] , int size){
+    for (int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
 
 int main(){
- cout<<"sameer"<<endl;   
+  int arr[5] = {41,343,22,331,52};
+    insetionSort(arr , 5);
+    printArray(arr , 5);
 }
