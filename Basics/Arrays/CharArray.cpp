5f456221f920c3ba8dd8e5d2d08dd25ed0ab77e9
Sameer <sameersattar1111@gmail.com>
2023-10-07 19:09:49 +0500

    char array  commit

--- Basics/Arrays/CharArray.cpp
+++ Basics/Arrays/CharArray.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int getLength(char arr[]){
+    int count = 0;
+    for (int i = 0; arr[i] != '\0'; i++){
+        count++;
+    }
+    return count;
+}
+
 int main(){
     char ch[10];
     cout<<"Enter your name"<<endl;
     cin>>ch;
-    ch[6] = '\0';
     cout<<"Your name is ";
-    cout<<ch;
+    cout<<ch<<" ";
+    cout<<getLength(ch);
 }
