8319fb15c18f286ddadcb3a8e5dc7f42da9a39c2
Sameer <sameersattar1111@gmail.com>
2023-10-08 11:20:25 +0500

    char array  commit

--- Basics/Arrays/CharArray.cpp
+++ Basics/Arrays/CharArray.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 using namespace std;
 
+// convert uppercaase to lowercase
+char toLowerCase(char ch){
+    if (ch >= 'a' && ch <= 'z'){
+        return ch;
+    }else{
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+    
+}
+// check palindrome or not 
+bool checkPalindrome(char arr[] , int n){
+    int s = 0;
+    int e = n-1;
+
+    while (s <= e){
+        if (toLowerCase(arr[s]) != toLowerCase(arr[e])){
+            return 0;
+        }else{
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
+
+//reverse the string
+void reverse(char arr[] , int n){
+    int s = 0;
+    int e = n-1;
+    while (s<=e){
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+        s++;
+        e--;
+    }
+    
+}
+
+// calculate the lenght of string
 int getLength(char arr[]){
     int count = 0;
     for (int i = 0; arr[i] != '\0'; i++){
@@ -10,10 +51,14 @@ int getLength(char arr[]){
 }
 
 int main(){
-    char ch[10];
+    char ch[20];
     cout<<"Enter your name"<<endl;
     cin>>ch;
     cout<<"Your name is ";
     cout<<ch<<" ";
-    cout<<getLength(ch);
+    int len = getLength(ch);
+    cout<<len<<endl;
+    reverse(ch , len);
+    cout<<ch<<" ";
+    cout<<"Palindrome or not "<<checkPalindrome(ch ,len)<<endl;
 }
