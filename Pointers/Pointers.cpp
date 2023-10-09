7e6dd31493b47018924ce812fc83d5a689e27a3c
Sameer <sameersattar1111@gmail.com>
2023-10-09 21:27:41 +0500

    pointers commit

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -21,4 +21,16 @@ int main()
 
     cout << "The address of d in p " << p << endl; // only print the addrress of d which is assign to p
     cout << "The value of p " << *p << endl;         // only print the value of d which is assign to p
+
+    int n1 = 9;
+    cout<<"the value of n1 "<<n1<<endl;
+    int a = n1;
+    a++;
+    cout<<"the value of n1 "<<n1<<endl;
+
+    int *p1 = &n1;
+    cout<<"the address of n1 "<<p1<<endl;
+    cout<<"the value of n1 before increament "<<*p1<<endl;
+    (*p1)++;
+    cout<<"the value of n1 after increament "<<*p1<<endl;
 }
