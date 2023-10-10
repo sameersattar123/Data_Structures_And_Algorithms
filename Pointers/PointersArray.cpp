f863418eace2da23e39862cef14803752eeb5c60
Sameer <sameersattar1111@gmail.com>
2023-10-10 17:46:28 +0500

    pointers arrays  commit

--- Pointers/PointersArray.cpp
+++ Pointers/PointersArray.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main(){
 
-int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+int arr[10] = {11,22,23,4,5,6,7,8,9,10};
 cout<<arr[9]<<endl;
+cout<<"the memory of first block "<<arr<<endl;
+cout<<"the memory of first block "<<&arr[0]<<endl;
+cout<<"the memory of first block "<<*arr<<endl;
+cout<<"the memory of first block "<<*(arr+1)<<endl;
+cout<<"the memory of first block "<<*(arr+2)<<endl;
+cout<<"the memory of first block "<<*arr+2<<endl;
+cout<<"the memory of first block "<<*arr+3<<endl;
+cout<<"the memory of first block "<<*arr+4<<endl;
 }
