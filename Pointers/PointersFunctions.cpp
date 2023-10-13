4192ace079798ccb62d97a1b13f7b99c05cf6250
Sameer <sameersattar1111@gmail.com>
2023-10-14 02:00:15 +0500

    double  pointers  commit

--- Pointers/PointersFunctions.cpp
+++ Pointers/PointersFunctions.cpp
@@ -6,10 +6,21 @@ void print(int *p){
     cout << *p + 1 << endl;
     cout << *p + 9 << endl;
 }
+int getSum(int *arr , int size){
+    cout<< sizeof(arr)<< endl;
+    int sum = 0;
+    for (int i = 0; i <= size; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
 
 int main(){
     int value = 6;
     int *p = &value;
 
-    print(p);
+    // print(p);
+
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    cout<< getSum(arr, 10)<<endl;
 }
