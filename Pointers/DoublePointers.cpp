639ac71ec190b06b3b05f42938875185eaa6163c
Sameer <sameersattar1111@gmail.com>
2023-10-14 02:20:58 +0500

    double  pointers  commit

--- Pointers/DoublePointers.cpp
+++ Pointers/DoublePointers.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
-    cout << "sameer"<< endl;
+    int i = 10;
+    int *p1 = &i;
+    int **p2 = &p1; 
+    int ***p3 = &p2;
+
+    cout << i << endl; 
+    cout << *p1 << endl; 
+    cout << **p2 << endl; 
+    cout << ***p3 << endl; 
 }
