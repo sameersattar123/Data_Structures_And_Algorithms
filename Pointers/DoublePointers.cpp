3a6122c24c418551043fc84b005579e8bc6ed8ad
Sameer <sameersattar1111@gmail.com>
2023-10-14 02:24:28 +0500

    double  pointers  commit

--- Pointers/DoublePointers.cpp
+++ Pointers/DoublePointers.cpp
@@ -7,8 +7,17 @@ int main(){
     int **p2 = &p1; 
     int ***p3 = &p2;
 
-    cout << i << endl; 
-    cout << *p1 << endl; 
-    cout << **p2 << endl; 
-    cout << ***p3 << endl; 
+    cout << i << endl; // 10
+    cout << *p1 << endl; // 10
+    cout << **p2 << endl; // 10
+    cout << ***p3 << endl; // 10
+
+    cout<< &i <<endl; // 0x61ff08
+    cout<< p1 <<endl; // 0x61ff08
+    cout<< *p2 <<endl; // 0x61ff08
+    cout<< **p3 <<endl; // 0x61ff08
+
+    cout << &p1 << endl;
+    cout << p2 << endl;
+    cout << p3 << endl;
 }
