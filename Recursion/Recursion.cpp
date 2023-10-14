2a1139ae1ef0394ccba32ff3e03ecd98cfbea27b
Sameer <sameersattar1111@gmail.com>
2023-10-14 20:52:10 +0500

    recursion  commit

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -13,7 +13,25 @@ int power(int n){
     if (n == 0){ // base condition 
         return 1;
     }
-    return 2 * power(n-1);
+    return 2 * power(n-1); // recursive relation
+}
+
+void counting1(int n){
+    if (n == 0){ // base condition
+        return;
+    }
+    counting1(n-1); // recursive relation
+
+    cout<<n<<endl; // processing
+}
+
+void counting2(int n){
+    if (n == 0){ // base condition
+        return;
+    }
+    cout<<n<<endl; // processing
+
+    counting2(n-1); // recursive relation
 }
 
 int main(){
@@ -21,9 +39,14 @@ int n;
 cout<<"Enter any number"<<endl;
 cin>>n;
 
-// int ans = factorial(n);
-// cout<<ans<<endl;
+int ans = factorial(n);
+cout<<ans<<endl;
 
 int ans = power(n);
 cout<<ans<<endl;
+
+counting1(n);
+
+counting2(n);
+
 }
---
+++ Recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+int ans = factorial(n);
+// cout<<ans<<endl;
+
+// int ans = power(n);
+// cout<<ans<<endl;
