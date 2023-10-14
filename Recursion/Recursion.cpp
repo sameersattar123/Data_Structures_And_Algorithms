0535e719c5a1560917b78cf412b00571d8d551ee
Sameer <sameersattar1111@gmail.com>
2023-10-14 20:17:46 +0500

    recursion  commit

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 int factorial(int n){
-    if (n == 0){
+    if (n == 0){ // base condition 
         return 1;
     }
-    return n * factorial(n-1);
+    return n * factorial(n-1); // Recusrive Relation // return key word is complusory and calling function itself is complusory
     
+}
+
+int power(int n){
+    if (n == 0){ // base condition 
+        return 1;
+    }
+    return 2 * power(n-1);
 }
 
 int main(){
@@ -14,6 +21,9 @@ int n;
 cout<<"Enter any number"<<endl;
 cin>>n;
 
-int ans = factorial(n);
+// int ans = factorial(n);
+// cout<<ans<<endl;
+
+int ans = power(n);
 cout<<ans<<endl;
 }
