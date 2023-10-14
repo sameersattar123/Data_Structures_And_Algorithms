4b63d14c97996914edd4e148e20d4ea9e334e074
Sameer <sameersattar1111@gmail.com>
2023-10-14 18:45:08 +0500

    recursion  commit

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int n){
+    if (n == 0){
+        return 1;
+    }
+    return n * factorial(n-1);
+    
+}
+
 int main(){
-cout<<"sameer"<<endl;
+int n;
+cout<<"Enter any number"<<endl;
+cin>>n;
+
+int ans = factorial(n);
+cout<<ans<<endl;
 }
