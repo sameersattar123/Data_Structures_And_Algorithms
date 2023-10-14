554d4b144c475f45ab85aeb06cd24ca713f08477
Sameer <sameersattar1111@gmail.com>
2023-10-15 00:05:48 +0500

    recursion questions  commit

--- Recursion/RecursionQuestions.cpp
+++ Recursion/RecursionQuestions.cpp
@@ -3,16 +3,32 @@ using namespace std;
 
 void reachHome(int src , int dest){
     cout<<"src "<<src<<" dest "<<dest<<endl;
-    if (src == dest){
+    if (src == dest){  // base cpnsition
         cout<<"ghar poucnh gaye"<<endl;
         return;
     }
-    src++;
-    reachHome(src , dest);
+     src++;  // proceesing
+    reachHome(src , dest); // recursive call
+}
+
+int fab(int n){
+    if (n == 0){ // base condition
+     return 0;
+    }
+    if ( n == 1){  // base condition
+        return  1;
+    }
+    return fab(n-1) + fab(n-2);  // recursive relation
+    
 }
 int main(){
 int src = 1;
 int dest = 10;
 
-reachHome(src, dest);
+reachHome(src, dest); // basic recursion question
+
+int n = 4;
+int ans = fab(n); // fabinacci series question
+cout<<ans<<endl;
+
 }
