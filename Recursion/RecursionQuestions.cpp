790eaa8687146f8416bce86bcc9b434b946eea03
Sameer <sameersattar1111@gmail.com>
2023-10-15 16:53:53 +0500

    recursion questions  commit

--- Recursion/RecursionQuestions.cpp
+++ Recursion/RecursionQuestions.cpp
@@ -21,6 +21,20 @@ int fab(int n){
     return fab(n-1) + fab(n-2);  // recursive relation
     
 }
+
+void sayDigits(int n , string arr[]){
+    if (n == 0){ // base condition
+        return ;
+    }
+
+    int digit = n % 10; // processing
+    n =  n / 10;
+
+    
+    sayDigits(n ,arr);  // recurcive relation
+    
+    cout<<arr[digit]<<" "; 
+}
 int main(){
 int src = 1;
 int dest = 10;
@@ -30,5 +44,17 @@ reachHome(src, dest); // basic recursion question
 int n = 4;
 int ans = fab(n); // fabinacci series question
 cout<<ans<<endl;
+
+int s;
+cin>>s;
+
+string arr[10] = {
+    "zero" , "one" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight" , "nine"
+};
+
+sayDigits(s, arr);
+
+
+
 
 }
--- Recursion/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-int ans = factorial(n);
-// cout<<ans<<endl;
-
-// int ans = power(n);
-// cout<<ans<<endl;
