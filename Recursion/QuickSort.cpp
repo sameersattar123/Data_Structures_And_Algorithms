b617b188bf3b5a7efaed583137d1f369476d04b6
Sameer <sameersattar1111@gmail.com>
2023-10-17 19:53:15 +0500

    OOP commit

--- Recursion/QuickSort.cpp
+++ Recursion/QuickSort.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
-int Partition(int arr[] , int s , int e){
+int Partition(int arr[], int s, int e)
+{
     int pivot = arr[s];
 
     // Counting the elements which is less than pivot
     int count = 0;
-    for (int i = s+1; i <= e; i++){
-        if (arr[i] <= pivot){
+    for (int i = s + 1; i <= e; i++)
+    {
+        if (arr[i] <= pivot)
+        {
             count++;
         }
     }
@@ -17,50 +20,58 @@ int Partition(int arr[] , int s , int e){
     arr[pivotIndex] = arr[s];
     arr[s] = swap;
 
-    //  left ans right wala part 
-    int i = s;  int j = e;
-    while (i < pivotIndex && j > pivotIndex){
-        while (arr[i] <= pivotIndex){
+    //  left ans right wala part
+    int i = s;
+    int j = e;
+    while (i < pivotIndex && j > pivotIndex)
+    {
+        while (arr[i] <= pivotIndex)
+        {
             i++;
         }
-        while (arr[j] > pivotIndex){
+        while (arr[j] > pivotIndex)
+        {
             j--;
         }
 
-        if (i < pivotIndex && j > pivotIndex){
+        if (i < pivotIndex && j > pivotIndex)
+        {
             int swap = arr[i];
-            arr[i]= arr[j];
+            arr[i] = arr[j];
             arr[j] = swap;
             i++;
             j--;
         }
-        
     }
-    
+
     return pivotIndex;
 }
-void quickSort(int arr[] , int s , int e){
+void quickSort(int arr[], int s, int e)
+{
 
     // Base Condition
-    if (s >= e){
+    if (s >= e)
+    {
         return;
     }
 
     // Partition
     int p = Partition(arr, s, e);
-    
-    // left partition sort 
-    quickSort(arr,s , p-1);
 
-    // Right partition sort 
-    quickSort(arr, p+1 , e);
+    // left partition sort
+    quickSort(arr, s, p - 1);
+
+    // Right partition sort
+    quickSort(arr, p + 1, e);
 }
-int main(){
-    int arr[6] = {2,33,1,32,22,3};
+int main()
+{
+    int arr[6] = {2, 33, 1, 32, 22, 3};
     int n = 6;
-    quickSort(arr , 0 , n-1);
-    for (int i = 0; i < n; i++){
-        cout<<arr[i]<<" ";
-    }cout<<endl;
-    
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
