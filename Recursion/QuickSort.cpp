170156f3875f4467106f7d03c3b137799e2367aa
Sameer <sameersattar1111@gmail.com>
2023-10-17 17:26:51 +0500

    quick sort  commit

--- Recursion/QuickSort.cpp
+++ Recursion/QuickSort.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 using namespace std;
 int Partition(int arr[] , int s , int e){
+    int pivot = arr[s];
 
+    // Counting the elements which is less than pivot
+    int count = 0;
+    for (int i = s+1; i <= e; i++){
+        if (arr[i] <= pivot){
+            count++;
+        }
+    }
+
+    // Place Pivot at right place
+    int pivotIndex = s + count;
+    int swap = arr[pivotIndex];
+    arr[pivotIndex] = arr[s];
+    arr[s] = swap;
+
+    //  left ans right wala part 
+    int i = s;  int j = e;
+    while (i < pivotIndex && j > pivotIndex){
+        while (arr[i] <= pivotIndex){
+            i++;
+        }
+        while (arr[j] > pivotIndex){
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex){
+            int swap = arr[i];
+            arr[i]= arr[j];
+            arr[j] = swap;
+            i++;
+            j--;
+        }
+        
+    }
+    
+    return pivotIndex;
 }
 void quickSort(int arr[] , int s , int e){
-    
+
     // Base Condition
     if (s >= e){
         return;
@@ -23,7 +59,7 @@ int main(){
     int arr[6] = {2,33,1,32,22,3};
     int n = 6;
     quickSort(arr , 0 , n-1);
-    for (int i = 0; i <= n; i++){
+    for (int i = 0; i < n; i++){
         cout<<arr[i]<<" ";
     }cout<<endl;
     
