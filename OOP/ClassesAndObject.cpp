5e5fd98f9b3ffd7510f9821cc3fb04b741ea2759
Sameer <sameersattar1111@gmail.com>
2023-10-18 00:53:06 +0500

    OOP commit

--- OOP/ClassesAndObject.cpp
+++ OOP/ClassesAndObject.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 using namespace std;
 
+class Hero {
+
+    private:
+    string name;
+
+    public:
+    int health;
+    char grade;
+
+    string getName(){
+        return name;
+    }
+    int getHealth(){
+        return health;
+    }
+    char getGrade(){
+        return grade;
+    }
+
+    void setName(string n){
+        name = n;
+    };
+    void setHealth(int h){
+        health = h;
+    };
+    void setGrade(char g){
+        grade = g;
+    };
+};
 int main(){
-    cout<<"sameer"<<endl;
+  Hero person;
+  person.setHealth(34);
+  person.setGrade('A');
+  person.setName("sameer");
+  cout<<person.getName()<<endl;
+  cout<<person.getHealth()<<endl; 
+  cout<<person.getGrade()<<endl;  
 }
