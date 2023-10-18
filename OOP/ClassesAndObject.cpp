0ae811f6e63e8d1428e4f2b56fc92ff923c63967
Sameer <sameersattar1111@gmail.com>
2023-10-18 21:43:22 +0500

    OOP commit

--- OOP/ClassesAndObject.cpp
+++ OOP/ClassesAndObject.cpp
@@ -4,38 +4,52 @@ using namespace std;
 class Hero {
 
     private:
-    string name;
+    string name;  // to access private acces modifier , we used getter and setter
 
     public:
     int health;
     char grade;
 
-    string getName(){
+    string getName(){  // getter
         return name;
     }
-    int getHealth(){
+    int getHealth(){ // getter
         return health;
     }
-    char getGrade(){
+    char getGrade(){ // getter
         return grade;
     }
 
-    void setName(string n){
+    void setName(string n){ // setter
         name = n;
     };
-    void setHealth(int h){
+    void setHealth(int h){ // setter
         health = h;
     };
-    void setGrade(char g){
+    void setGrade(char g){ // setter
         grade = g;
     };
 };
 int main(){
+  // Static Allocation
   Hero person;
+
+  // Access Person using Static Allocation 
   person.setHealth(34);
   person.setGrade('A');
   person.setName("sameer");
   cout<<person.getName()<<endl;
   cout<<person.getHealth()<<endl; 
-  cout<<person.getGrade()<<endl;  
+  cout<<person.getGrade()<<endl;
+
+  // Dynamic Allocation
+  Hero *Person = new Hero;
+
+ // Access Person using Dynamics Allocation
+  Person->setHealth(344);
+  Person->setGrade('B');
+  Person->setName("sameera");
+  cout<<Person->getName()<<endl;
+  cout<<Person->getHealth()<<endl; 
+  cout<<Person->getGrade()<<endl;
 }
