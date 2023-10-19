a6c58071dbc39b4f512dd29691b5d4cc93ef09d7
Sameer <sameersattar1111@gmail.com>
2023-10-20 03:46:34 +0500

    Inheritance  commit

--- OOP/Inheritance.cpp
+++ OOP/Inheritance.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
+class Human{
+    public:
+    int age;
+    string name;
+
+    void setAge(int age){
+        this->age = age;
+    }
+    void setName(string name){
+        this->name = name;
+    }
+};
+
+class Male: public Human{
+    public:
+    string color;
+
+    void sleep(){
+        cout<<"I am sleeping"<<endl;
+    }
+     void setColor(string color){
+        this->color = color;
+    }
+};
+
 int main(){
-    cout<<"sameer"<<endl;
+  Male sameer;
+  sameer.setAge(34);  
+  sameer.setName("sameer sattar");
+  sameer.setColor("green");
+  cout<<sameer.age<<endl;  
+  cout<<sameer.name<<endl;
+  cout<<sameer.color<<endl;
+  sameer.sleep();  
 }
