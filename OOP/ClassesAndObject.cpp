e3ef72c6c830f1a7fd9e3af22833fcc77cdba882
Sameer <sameersattar1111@gmail.com>
2023-10-19 22:57:52 +0500

    OOP static keyword  commit

--- OOP/ClassesAndObject.cpp
+++ OOP/ClassesAndObject.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Hero
-{
+class Hero{
 
 private:
     string name; // to access private acces modifier , we used getter and setter
@@ -11,40 +10,32 @@ public:
     int health;
     char grade;
 
-    string getName()
-    { // getter
+    string getName(){ // getter
         return name;
     }
-    int getHealth()
-    { // getter
+    int getHealth(){ // getter
         return health;
     }
-    char getGrade()
-    { // getter
+    char getGrade(){ // getter
         return grade;
     }
 
-    void setName(string n)
-    { // setter
+    void setName(string n){ // setter
         name = n;
     };
-    void setHealth(int h)
-    { // setter
+    void setHealth(int h){ // setter
         health = h;
     };
-    void setGrade(char g)
-    { // setter
+    void setGrade(char g){ // setter
         grade = g;
     };
-    Hero(int health, char grade, string name)
-    { // paramatric Contructor
+    Hero(int health, char grade, string name){ // paramatric Contructor
         cout << "level , grade , name Constructor" << endl;
         this->grade = grade;
         this->health = health;
         this->name = name;
     }
-    Hero(char grade, string name)
-    { // paramatric Contructor
+    Hero(char grade, string name){ // paramatric Contructor
         cout << "grade and name Constructor" << endl;
         this->grade = grade;
         this->name = name;
@@ -65,13 +56,29 @@ public:
         cout << "Copy Constructor" << endl;
     }
 
-    void print()
-    {
+    void print(){
         cout << "print " << name << endl;
     }
 };
-int main()
-{
+
+class StaticHero{ // Concept of Static keyword
+    public:
+    static string Name;  // static name
+    static int age;  // static age
+
+    static string StaticPersonName(){ // static function
+        cout<< Name << endl; // static name
+    } 
+    static int StaticPersonAge(){ // static function
+        cout<< age << endl; // static age
+    } 
+};
+
+string StaticHero::Name = "sameeeeeer";
+int StaticHero::age = 222;
+
+int main(){
+
     // Static Allocation
     Hero person;
     //   Hero person("sameer"); // paramatric Contructor
@@ -105,5 +112,16 @@ int main()
     cout << Person->getGrade() << endl;
 
     delete Person; // for calling destructor 
-    
+
+    // Access Static varibles 
+    cout<<"s"<<endl;
+    cout<<StaticHero::Name<<endl;
+    cout<<"s"<<endl;
+    cout<<StaticHero::age<<endl;
+
+    // Access Static Function
+    cout<<"s"<<endl;
+    cout<<StaticHero::StaticPersonAge()<<endl;
+    cout<<"s"<<endl;
+    cout<<StaticHero::StaticPersonName()<<endl;
 }
