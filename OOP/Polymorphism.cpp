8ab8590b1b49ae60992d7969ffdcc5b7a4a81068
Sameer <sameersattar1111@gmail.com>
2023-10-23 23:54:59 +0500

    polymorphism  commit

--- OOP/Polymorphism.cpp
+++ OOP/Polymorphism.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class A {
+    public:
+    void sayHello(){ // overloading
+        cout<<"hello"<<endl;
+    }
+    void sayHello(string name){ // overloading
+        cout<<"hello " + name<<endl;
+    }
+    void sayHello(char name){ // overloading
+        cout<<"hello "<< name<<endl;
+    }
+};
+
 int main(){
-    
+  A obj;
+  obj.sayHello();   // overloading
+  obj.sayHello("sameer"); // overloading  
+  obj.sayHello('r');  // overloading
 }
