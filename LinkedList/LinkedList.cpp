0b8d9848b0329057198b614f70632645868c0db2
Sameer <sameersattar1111@gmail.com>
2023-10-25 23:57:48 +0500

    Linked list  commit

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+
 // Linked List is a linear Data Structure which contain collections of node 
 // one node is data and other node containe address of other node
 
@@ -16,8 +17,30 @@ class Node{
     }
 };
 
+    void InsertAtHead(Node* &head , int d){
+        Node* temp = new Node(d);
+        temp -> next = head;
+        head = temp;
+    }
+
+    void print(Node* &head ){
+        Node* temp  = head;
+        while (temp != NULL){
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+
 int main(){
    Node* node1 = new Node(10);
    cout<<node1->data<<endl;
    cout<<node1->next<<endl;
+
+   Node* head = node1;
+   print(head);
+
+   InsertAtHead(head , 12);
+    
+    print(head);
 }
