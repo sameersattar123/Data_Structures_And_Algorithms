f7666e16c455137d295754845fb6b87377f7dfd1
Sameer <sameersattar1111@gmail.com>
2023-10-28 00:01:13 +0500

    Doubly  Linked list  commit

--- LinkedList/DoubleLinkedList.cpp
+++ LinkedList/DoubleLinkedList.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"sameer"<<endl;
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    // Constructor
+    Node(int d)
+    {
+        this->data = d;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+// traversing of linked list
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data;
+        temp = temp->next;
+    }
+    cout << endl;
+};
+
+// lenght of linked list
+int getLenght(Node *head)
+{
+    int lenght = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        lenght++;
+        temp = temp->next;
+    }
+    return lenght;
+};
+
+int main()
+{
+    Node *node1 = new Node(100);
+    Node *head = node1;
+    print(head);
+    cout << getLenght(head) << endl;
+
+    return 0;
 }
