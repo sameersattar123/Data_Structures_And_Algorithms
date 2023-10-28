3fe033b8f3ef9b38f7514d73d217ef4ae0f228c1
Sameer <sameersattar1111@gmail.com>
2023-10-29 01:47:09 +0500

    Queue  commit

--- Queue/QueueWithSTL.cpp
+++ Queue/QueueWithSTL.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main(){
-    cout<<"sameer"<<endl;
+    queue <int> q;
+    q.push(100);
+
+    cout<<"front of queue is "<<q.front()<<endl;
+    q.push(200);
+
+    cout<<"front of queue is "<<q.front()<<endl;
+
+    q.push(300);
+
+    cout<<"front of queue is "<<q.front()<<endl;
+
+    cout<<"The size of queue is "<<q.size()<<endl;
+
+    q.pop();
+
+    cout<<"The size of queue is "<<q.size()<<endl;
+
+    q.pop();
+
+    cout<<"The size of queue is "<<q.size()<<endl;
+
+    q.pop();
+
+    cout<<"The size of queue is "<<q.size()<<endl;
+
+    if (q.empty()){
+        cout<<"Empty"<<endl;
+    } else {
+        cout<<"Not Empty"<<endl;
+    }
+    
+
 }
