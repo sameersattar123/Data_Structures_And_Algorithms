8b6f7599fcbb40d6b6dc2d1ebe1f29b21ff74f1b
Sameer <sameersattar1111@gmail.com>
2023-10-31 23:01:28 +0500

    Queue  commit

--- Queue/CircularQueue.cpp
+++ Queue/CircularQueue.cpp
@@ -8,6 +8,7 @@ class CircularQueue{
     int size;
     int rear;
 
+    // Constructor
     CircularQueue(int n){
         size = 1000;
         front = -1;
@@ -15,25 +16,38 @@ class CircularQueue{
         arr = new int[size];
     }
 
+    // Push Operation
     bool enqueue(int value){
-        if ((front == 0 && rear == (size-1) || ( rear == (front -1) % (size -1)))){
+        if ((front == 0 && rear == (size-1) || ( rear == (front -1) % (size -1)))){ // Queue is full
             cout<<"Queue is Full"<<endl;
             return false;
-        } else if(front == -1){
+        } else if(front == -1){ // Np element is push already
             rear = front = 0;
-            arr[rear] = value;
-        } else if ((rear == (size -1)) && (front != 0)){
+        } else if ((rear == (size -1)) && (front != 0)){ // front is in center and rear is in last
             rear = 0;
-            arr[rear] = value;
         } else {
             rear++;
-            arr[rear] = value;
         }
+            arr[rear] = value;
+            return true;
         
     }
 
-    bool dequeue(){
-
+    int dequeue(){
+        if (front == -1 ){ // Queue is empty
+            cout<<"Queue is Empty"<<endl;
+            return -1;
+        }
+        int ans = arr[front];
+        arr[front] = -1; 
+        if (rear == front){ // first element
+                rear = front = -1;
+        } else if (front == size -1) { // last element
+            front == 0;
+        } else { 
+            front++;
+        }
+        return ans;   
     }
 };
 
