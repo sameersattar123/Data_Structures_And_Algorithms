824bf180ef4fc42abe3e1490d3876507923a6d65
Sameer <sameersattar1111@gmail.com>
2023-11-01 10:04:24 +0500

    Queue  commit

--- Queue/DoubleEndedQueue/DoubleEndedQueueWithSTL.cpp
+++ Queue/DoubleEndedQueue/DoubleEndedQueueWithSTL.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main(){
-    cout<<"sameer"<<endl;
+    deque<int> d;
+    d.push_front(10);
+    d.push_back(45);
+
+    cout<<"front element is "<<d.front()<<endl;
+    cout<<"back element is "<<d.back()<<endl;
+
+    d.pop_front();
+
+    cout<<"front element is "<<d.front()<<endl;
+    cout<<"back element is "<<d.back()<<endl;
+
+    d.pop_back();
+
+
+    if(d.empty()){
+        cout<<"dequeue is empty"<<endl;
+    } else {
+        cout<<"dequeue is not empty"<<endl;
+    }
 }
