1c6acfc4d64a958c9708c65aa52aa8fd8c8f2038
Sameer <sameersattar1111@gmail.com>
2023-11-03 07:57:22 +0500

    Tree  commit

--- Tree/BinarySearchTree.cpp
+++ Tree/BinarySearchTree.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void takeInput(){
+    
+}
+
 int main(){
-    cout<<"sameer"<<endl;
+    Node* root = NULL;
+    cout<<"Enter the data to create BST "<<endl;
+
+    takeInput();  
 }
