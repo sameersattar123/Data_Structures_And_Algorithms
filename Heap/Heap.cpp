c5a40868b730657ae8ccd2861d8d2bb8af31cde2
Sameer <sameersattar1111@gmail.com>
2023-11-05 09:28:28 +0500

    Heap  commit

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -74,6 +74,32 @@ class heap {
     }
 };
 
+// Heapify Algorithm
+void heapify(int arr[] , int i , int  n){
+
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i+1;
+
+    if (left < n && arr[largest] < arr[left]){
+        largest = left;
+    }
+
+    if (right < n && arr[largest] < arr[right]){
+        largest = right;
+    }
+
+    if (largest != i){
+        int temp = arr[largest];
+        arr[largest] = arr[i];
+        arr[i] = temp;
+        heapify(arr,n,largest);
+    }
+    
+
+    
+}
+
 int main(){
 
     heap h;
@@ -88,8 +114,24 @@ int main(){
     // deletion
     h.deleteFromHeap();
     h.deleteFromHeap();
-
+    // print
     h.print();
+
+    // Heapify ALgorithm
+    
+    int arr[6] = {-1, 54 ,53,55 , 52 , 50};
+    int n = 5;
+    for (int i = n/2; i > 0; i--){
+        heapify(arr,n,i);
+    }
+
+    cout<<"Printing the array now "<<endl;
+
+    for (int i = 1; i <= n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+    
+    
 
     return 0;
 }
